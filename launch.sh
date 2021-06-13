99e53764adb6ec8bd824d2291fa069725bff51f2
Samuel Lord <sllord@icloud.com>
2021-06-12 20:32:54 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@ wget https://github.com/sparksammy/callienaps-nw/blob/master/builds/callienaps_b
 echo Launching...
 cd lib2procV1
 chmod +x lib2proc
-sudo ./lib2proc $(pgrep '^RobloxPlayer$') ../callienaps.dylib
+sudo ./lib2proc $(pgrep '^RobloxPlayer$' | head -1) ../callienaps.dylib
