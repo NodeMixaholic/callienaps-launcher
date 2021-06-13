68b90008e9fb6313848db9463a0a63f5a61b08f1
Samuel Lord <sllord@icloud.com>
2021-06-12 21:19:50 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,7 @@
 echo Updating...
-wget https://github.com/sparksammy/callienaps-nw/blob/master/builds/callienaps_blue_main.dylib?raw=true -O callienaps.dylib
+  wget https://github.com/sparksammy/callienaps-nw/blob/master/builds/callienaps_blue_main.dylib?raw=true -O callienaps.dylib
 echo Launching...
-DYLD_INSERT_LIBRARIES=callienaps.dylib /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
+  git clone https://github.com/sparksammy/lib2proc-fluxus
+  cd lib2proc-fluxus
+  chmod +x lib2proc
+  sudo ./lib2proc $(pgrep '^RobloxPlayer$' | head -1) ../callienaps.dylib
