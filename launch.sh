8fa84f18c9461e06ce2b0d9ba08ea9e254df59df
Samuel Lord <sllord@icloud.com>
2021-06-12 23:59:53 -0500

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 echo Updating...
-  wget https://github.com/sparksammy/callienaps-nw/blob/master/builds/callienaps_blue_main.dylib?raw=true -O callienaps.dylib
+  git clone https://github.com/sparksammy/callienaps-nw
 echo Launching...
   git clone https://github.com/sparksammy/lib2proc-fluxus
+  chmod +x lib2proc-fluxus/lib2proc
   cd lib2proc-fluxus
-  chmod +x lib2proc
-  sudo ./lib2proc $(pgrep '^RobloxPlayer$' | head -1) ../callienaps.dylib
+  sudo ./lib2proc $(pgrep '^RobloxPlayer$' | head -1) ../callienaps-nw/builds/callienaps_blue_main.dylib
 
